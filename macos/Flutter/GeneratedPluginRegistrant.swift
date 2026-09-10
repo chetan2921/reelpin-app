c018ffa10e8e261fa918fecf6bdcee7221e388b5
chetan2921 <chetanjain2921@gmail.com>
2026-09-11 01:11:04 +0530

    Add the packages the chat needs
    fl_chart draws the chart blocks in answers; image_picker and file_picker let
    a question carry a photo or a file. The lock files and plugin registrants
    follow from those three, and Info.plist gains the camera and photo library
    usage strings iOS requires before either picker can open.
    
    Signed-off-by: chetan2921 <chetanjain2921@gmail.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import file_picker
+import file_selector_macos
 import firebase_analytics
 import firebase_core
 import firebase_crashlytics
@@ -21,6 +23,8 @@ import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseCrashlyticsPlugin"))
